e9367d39929b476cb7ed99109c2145caa2b9ef19
krish319w-boop <krish319w@gmail.com>
2026-04-26 18:31:09 +0300

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCTUb72QTPGr2yzrJCFgJNBWSI2D9gt_Q8",
+  authDomain: "hoda-portflio.firebaseapp.com",
+  projectId: "hoda-portflio",
+  storageBucket: "hoda-portflio.firebasestorage.app",
+  messagingSenderId: "881971239680",
+  appId: "1:881971239680:web:770569ab484b92977a31e5",
+  measurementId: "G-BLF0R78SSM"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+const auth = firebase.auth();
